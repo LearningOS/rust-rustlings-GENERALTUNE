4f3c6544eb587ba653abddc08759a4e2e031f6eb
GENERALTUNE <577174042@qq.com>
2023-04-21 21:27:59 +0800

    feat: 第五次提交

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,12 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -38,6 +40,13 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        // let { ChangeColor, Echo, Move, Quit } = message;ss
+        match message {
+            Message::ChangeColor(color) => self.change_color(color),
+            Message::Echo(text) => self.echo(text),
+            Message::Move(p) => self.move_position(p),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
