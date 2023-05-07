f65eadff9c23333a9d3ce8865d3901deac25a4e7
GENERALTUNE <577174042@qq.com>
2023-05-07 08:05:40 +0800

    fix: 尝试修复

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -14,7 +14,7 @@ fn main() {
     let pi = PI;
     let radius = 5.00f32;
 
-    let area = pi * f32::powi(radius, 2);
+    let area = PI * f32::powi(radius, 2);
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
